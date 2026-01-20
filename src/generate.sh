3f62c027e4d96eb0d44d5d9d36b649abc67878bf
Roman Metlinskyi <gmail@metlinskyi.com>
2026-01-20 11:28:44 +0100

    update

---
+++ src/generate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd src/
+
+# Generate README.md 
+copilot --allow-tool 'write' -p 
+"
+    generate readme.md based on data in src/data.yaml and template in src/template.md
+"
+# Generate index.html
+copilot --allow-tool 'write' -p 
+"
+    generate index.html based on data in src/data.yaml and template in src/template.html, 
+    css should be support convert pdf,
+    do not create a header for the about section,
+    slightly highlight technical skills for description and about using gray shade without bold,
+    wrap new lines into paragraph only for about text
+"  
+# Generate PDF from index.html
+wkhtmltopdf -s A4 index.html '../Roman Metlinskyi CV.pdf'
+cd ..
+
---
+++ src/template.md
@@ -0,0 +1,26 @@
+# {givenName} {familyName}
+
+**{jobTitle}** 
+
+📍 {address}  
+📧 {email}
+
+## 
+{about}
+
+## Experience
+
+### {company}
+**{title}**  
+*{period}*  
+📍 {location}
+
+{description}
+
+
+## Education
+
+### {institution}
+- **name**, *period*
+
+## Languages
